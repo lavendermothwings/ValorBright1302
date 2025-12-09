0c388626c83251a7d5b1583d525cab44074b1454
Valor Bright <lavendermothwings>
2025-12-08 22:57:10 -0500

    Made a AddComicWindow
    1) handles name for comic
    2) handles number for comic
    3) handles list of comics

--- code/Project3Version5ValorBright/src/main/java/edu/westga/cs1302/task_tracker/viewmodel/AddComicWindowViewModel.java
+++ code/Project3Version5ValorBright/src/main/java/edu/westga/cs1302/task_tracker/viewmodel/AddComicWindowViewModel.java
@@ -9,41 +9,70 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 
+/** Controller for AddComicWindow
+ * 
+ * @author Valor Bright
+ * @version Fall 2025
+ */
 public class AddComicWindowViewModel {
+	private MainWindowViewModel vm = new MainWindowViewModel();
 	private StringProperty addComicName;
 	private StringProperty addComicNumber;
 	private ListProperty<ComicBook> comicBooks;
 	
-	MainWindowViewModel vm = new MainWindowViewModel();
-	
-	
+	/** Initialize the AddComicWindowViewModel
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 */
 	public AddComicWindowViewModel() {
 		this.addComicName = new SimpleStringProperty("");
 		this.addComicNumber = new SimpleStringProperty("");
 		this.comicBooks = new SimpleListProperty<ComicBook>(FXCollections.observableList(new ArrayList<ComicBook>()));
 	}
 	
+	/** Returns the comic name property
+	 * 
+	 * @return addComicName name of comic
+	 */
 	public StringProperty getAddComicName() {
 		return this.addComicName;
 	}
 	
+	/** Returns the comic number property
+	 * 
+	 * @return addComicNumber
+	 */
 	public StringProperty getAddComicNumber() {
 		return this.addComicNumber;
 	}
 	
+	/** Removes comic
+	 * 
+	 */
 	public void removeComic() {
 		
 	}
 	
+	/** Cancels Adding Comic
+	 * 
+	 */
 	public void cancelAddComic() {
 		//close window
 	}
 	
+	/** Confirms Adding Comic
+	 * 
+	 */
 	public void confirmAddComic() {
 		ComicBook comic = new ComicBook(this.addComicName.get(), this.getAddComicNumber().get());
 		this.vm.addComic(comic);
 	}
 	
+	/** Gets comic book list
+	 * 
+	 * @return comicBooks list
+	 */
 	public ListProperty<ComicBook> getComicBooks() {
 		return this.comicBooks;
 	}
